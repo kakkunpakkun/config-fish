71acdffbed692c2fd28a2d8eecd23481c1b90b62
kakkunpakkun <11kaku@gmail.com>
2020-01-04 17:26:29 +0900

    do not show ruby version

--- config.fish
+++ config.fish
@@ -10,7 +10,12 @@ alias gst='git status'
 alias ga='git add'
 alias gd='git diff'
 alias gc='git commit'
-
 alias git_current_branch="git branch | grep \* | cut -d ' ' -f2"
 alias ggpull='git pull origin (git_current_branch)'
 alias ggpush='git push origin (git_current_branch)'
+
+# cd to ~/Workspace
+alias cdw='cd ~/Workspace'
+
+# set bobthefish not to display ruby
+set -g theme_display_ruby no
