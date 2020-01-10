7827f026fc26439410ed6e211dde569b738df02c
kakkunpakkun <kaku@freee.co.jp>
2020-01-10 18:46:43 +0900

    add lssh and git command shortcuts

--- config.fish
+++ config.fish
@@ -1,6 +1,9 @@
 # set PATH
 set PATH $HOME/.cargo/bin $HOME/.tfenv/bin $HOME/.rbenv/bin $PATH
 
+# rbenv
+status --is-interactive; and source (rbenv init -|psub)
+
 # use exa instead of ls
 alias ls='exa'
 
@@ -10,3 +13,17 @@ alias gst='git status'
 alias ga='git add'
 alias gd='git diff'
 alias gc='git commit'
+alias git_current_branch="git branch | grep \* | cut -d ' ' -f2"
+alias ggpull='git pull origin (git_current_branch)'
+alias ggpush='git push origin (git_current_branch)'
+
+# cd to ~/Workspace
+alias cdw='cd ~/Workspace'
+
+# set bobthefish not to display ruby
+set -g theme_display_ruby no
+
+# hooks for direnv
+function __direnv_export_eval --on-event fish_prompt;
+    "/usr/local/bin/direnv" export fish | source;
+end
---
+++ functions/direnv_hook.fish
@@ -0,0 +1,4 @@
+
+function __direnv_export_eval --on-event fish_prompt;
+	"/usr/local/bin/direnv" export fish | source;
+end
