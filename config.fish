1f312bbb46a8a5648c8be7ade3667b53290bc620
kakkunpakkun <kakkunpakkun@gmail.com>
2020-04-18 19:22:41 +0900

    use fisher

--- config.fish
+++ config.fish
@@ -1,12 +1,16 @@
+set -x DENO_INSTALL /Users/kp/.local
 # set PATH
 set -x PATH $HOME/.cargo/bin $HOME/.tfenv/bin $PATH
-set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+set -x PATH $HOME/.rbenv/bin $PATH
+set -x PATH $DENO_INSTALL/bin $PATH
 
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
 
 # use exa instead of ls
 alias ls='exa'
+alias l='ls'
+alias la='ls -lag'
 
 # git status
 alias g='git'
@@ -21,10 +25,16 @@ alias ggpush='git push origin (git_current_branch)'
 # cd to ~/Workspace
 alias cdw='cd ~/Workspace'
 
-# set bobthefish not to display ruby
-set -g theme_display_ruby no
-
 # hooks for direnv
 function __direnv_export_eval --on-event fish_prompt;
     "/usr/local/bin/direnv" export fish | source;
 end
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/kp/google-cloud-sdk/path.fish.inc' ]; . '/Users/kp/google-cloud-sdk/path.fish.inc'; end
